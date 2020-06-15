58090abfedac8bbc6248a7c6ac76812ead354801
Laszlo Pusok <laszlo.pusok@bitrise.io>
2020-06-15 11:51:04 +0200

    Initial Commit

---
+++ sample.mac/ContentView.swift
@@ -0,0 +1,23 @@
+//
+//  ContentView.swift
+//  sample.mac
+//
+//  Created by lpusok on 2020. 06. 15..
+//  Copyright © 2020. bitrise. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+            .frame(maxWidth: .infinity, maxHeight: .infinity)
+    }
+}
+
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
